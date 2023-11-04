51efbee24a0b297d7c3dec98f9f832cc37f3246e
Iomegan <dwitt.kiel@icloud.com>
2023-11-04 13:26:54 +0100

    inital commit

--- Reorder Nested List/ContentView.swift
+++ Reorder Nested List/ContentView.swift
@@ -5,20 +5,68 @@
 //  Created by Daniel Witt on 04.11.2023.
 //
 
+import CoreTransferable
 import SwiftUI
+import UniformTypeIdentifiers.UTType
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            OutlineGroup(data, children: \.children) { item in
+                ItemView(fileItem: item)
+                    .id(item)
+                    .tag(item)
+            }
         }
-        .padding()
+        .listStyle(SidebarListStyle())
     }
 }
 
-#Preview {
-    ContentView()
+struct ItemView: View {
+    let fileItem: FileItem
+    var body: some View {
+        let dropDelegate = ExampleDropDelegat()
+        Text("\(fileItem.description)")
+            .draggable(fileItem.name)
+            .onDrop(of: [.plainText], delegate: dropDelegate)
+    }
 }
+
+class ExampleDropDelegat: DropDelegate {
+    func performDrop(info: DropInfo) -> Bool {
+        print("It works! - but only on macOS :(")
+        return true
+    }
+}
+
+struct FileItem: Hashable, Identifiable, CustomStringConvertible, Transferable {
+    var id: Self { self }
+    var name: String
+    var children: [FileItem]? = nil
+    var description: String {
+        switch children {
+            case nil:
+                return "📄 \(name)"
+            case .some(let children):
+                return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
+        }
+    }
+
+    public static var transferRepresentation: some TransferRepresentation {
+        ProxyRepresentation { item in
+            item.name
+        }
+    }
+}
+
+let data =
+    FileItem(name: "users", children:
+        [FileItem(name: "user1234", children:
+            [FileItem(name: "Photos", children:
+                [FileItem(name: "photo001.jpg"),
+                 FileItem(name: "photo002.jpg")]),
+            FileItem(name: "Movies", children:
+                [FileItem(name: "movie001.mp4")]),
+            FileItem(name: "Documents", children: [])]),
+        FileItem(name: "newuser", children:
+            [FileItem(name: "Documents", children: [])])])
