e21a7508a76238424c97cb3c4ca0d45655908c9e
Iomegan <dwitt.kiel@icloud.com>
2023-11-04 12:46:52 +0100

    Initial Commit

---
+++ Reorder Nested List/Reorder_Nested_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reorder_Nested_ListApp.swift
+//  Reorder Nested List
+//
+//  Created by Daniel Witt on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Reorder_Nested_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
